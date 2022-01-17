94cff51f670d408e291dbe3317fa33f34ddc8b22
brakesh <bandarupallirakesh@gmail.com>
2022-01-17 22:53:24 +0530

    Shared library updated

--- route-table.tf
+++ route-table.tf
@@ -12,3 +12,13 @@ resource "aws_route_table" "private-table" {
     ENV=var.env
   }
 }
+resource "aws_route_table_association" "public" {
+  count=length(aws_subnet.public_subnets)
+  subnet_id      = aws_subnet.public_subnets.*.id[count.index]
+  route_table_id = aws_route_table.public-table.id
+}
+resource "aws_route_table_association" "private" {
+  count=length(aws_subnet.private_subnets)
+  subnet_id      = aws_subnet.public_subnets.*.id[count.index]
+  route_table_id = aws_route_table.private-table.id
+}
