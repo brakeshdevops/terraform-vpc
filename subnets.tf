d57a7524f0f05a9412b923835e073338c7e6951e
brakesh <bandarupallirakesh@gmail.com>
2022-01-17 22:01:07 +0530

    Shared library updated

---
+++ subnets.tf
@@ -0,0 +1,18 @@
+resource "aws_subnet" "public_subnets" {
+  count = length(var.PUBLIC_VPC_CIDR)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.PUBLIC_VPC_CIDR[count.index]
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+resource "aws_subnet" "private_subnets" {
+  count = length(var.PRIVATE_VPC_CIDR)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.PRIVATE_VPC_CIDR[count.index]
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
--- vars.tf
+++ vars.tf
@@ -1,2 +1,4 @@
 variable "VPC_CIDR" {}
 variable "env" {}
+variable "PUBLIC_VPC_CIDR" {}
+variable "PRIVATE_VPC_CIDR" {}
