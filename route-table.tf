7138e31c246c6516280608e9b04086f805a9979a
brakesh <bandarupallirakesh@gmail.com>
2022-01-17 22:39:18 +0530

    Shared library updated

---
+++ route-table.tf
@@ -0,0 +1,14 @@
+resource "aws_route_table" "public-table" {
+vpc_id= aws_vpc.main.id
+  tags= {
+    Name = "public-rt"
+    ENV=var.env
+  }
+}
+resource "aws_route_table" "private-table" {
+  vpc_id= aws_vpc.main.id
+  tags= {
+    Name = "private-rt"
+    ENV=var.env
+  }
+}
