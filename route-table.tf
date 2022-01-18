57a30bacd62abd720ca321a11f903421526d7c9b
brakesh <bandarupallirakesh@gmail.com>
2022-01-18 15:07:52 +0530

    Shared library updated

--- route-table.tf
+++ route-table.tf
@@ -33,3 +33,18 @@ resource "aws_route" "private-ngw-route" {
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id=aws_nat_gateway.ngw.id
 }
+resource "aws_route" "public-peer-route" {
+  route_table_id            = aws_route_table.public-table.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer-connection.id
+}
+resource "aws_route" "private-peer-route" {
+  route_table_id            = aws_route_table.private-table.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer-connection.id
+}
+resource "aws_route" "default-rtable-route" {
+  route_table_id            = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block    = var.VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer-connection.id
+}
